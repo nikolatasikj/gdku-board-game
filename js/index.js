a6b5adab84c8b6358e6d1afc588ab64b01abaded
Nikola Tasikj <nikola.tasikj@webfactory.mk>
2023-06-12 23:54:45 +0200

    Add more questions

--- js/index.js
+++ js/index.js
@@ -3,12 +3,16 @@ import {questions} from "./questions.js";
 let fieldNumber = 0;
 let spins = 0;
 let points = 0;
-const tableGrid = document.querySelector('#table-grid')
+let playerName = '';
+const tableGrid = document.querySelector('#tableGrid')
 const diceEl = document.querySelector('#dice');
-const rollBtn = document.querySelector('#roll-btn');
-const iconEl = document.querySelector('#user-icon');
-const questionModal = document.querySelector('#question');
+const rollBtn = document.querySelector('#rollBtn');
+const iconEl = document.querySelector('#userIcon');
+const modal = document.querySelector('#modal');
 const messageEl = document.querySelector('#message');
+const resetBtn = document.querySelector('#resetBtn');
+
+resetBtn.onclick = () => resetGame();
 rollBtn.onclick = () => rollDice();
 
 // Init HTML table fields
@@ -30,7 +34,61 @@ const increaseFieldNumber = (idx) => {
     iconEl.style.top = `${fieldPosition.bottom}px`;
 }
 
-increaseFieldNumber(0)
+const saveUser = () => {
+    const player = {
+        spins,
+        points,
+        fieldNumber,
+        playerName
+    }
+
+    localStorage.setItem('gdku-board-game', JSON.stringify(player))
+}
+
+const checkUser = () => {
+    const player = JSON.parse(localStorage.getItem('gdku-board-game'))
+
+    if (player) {
+        playerName = player.playerName;
+        fieldNumber = player.fieldNumber;
+        points = player.points;
+        spins = player.spins;
+        document.querySelector('#points').innerText = points;
+        document.querySelector('#spins').innerText = spins;
+
+        if (fieldNumber === questions.length + 1) {
+            rollBtn.disabled = true;
+            rollBtn.children[0].innerText = 'You won!'
+        }
+    } else {
+        const inputWrapper = document.createElement('div');
+        inputWrapper.classList.add('player-input-wrapper')
+        const label = document.createElement('label');
+        label.innerText = 'Full name: '
+        const input = document.createElement('input')
+        input.placeholder = 'Enter your full name';
+        const playBtn = document.createElement('button')
+        playBtn.id = 'playBtn'
+        playBtn.innerText = 'Play';
+        playBtn.onclick = () => {
+            if (input.value) {
+                playerName = input.value
+                saveUser();
+                modal.style.display = 'none';
+                modal.innerHTML = '';
+            }
+        }
+
+        inputWrapper.append(label)
+        inputWrapper.append(input)
+        modal.append(inputWrapper)
+        modal.append(playBtn)
+        modal.style.display = 'flex';
+    }
+}
+
+checkUser();
+increaseFieldNumber(0);
 
 const rollDice = () => {
     spins++;
@@ -45,12 +103,23 @@ const rollDice = () => {
 
     setTimeout(() => hideDice(), 2000)
     if (fieldNumber + newNumber === questions.length + 1) {
-        setTimeout(() => showMessage('You won!', true), 2000)
+        setTimeout(() => {
+            rollBtn.children[0].innerText = 'You won!';
+            showMessage('You won!', true)
+        }, 2000)
         increaseFieldNumber(newNumber);
+        saveUser();
+        savePlayerToAPI();
     } else if (fieldNumber + newNumber > questions.length) {
-        setTimeout(() => showMessage(`You need ${questions.length - fieldNumber + 1} to win!`, false, 1900), 2000)
+        setTimeout(() => {
+            rollBtn.disabled = false;
+            showMessage(`You need ${questions.length - fieldNumber + 1} to win!`, false, 1900)
+        }, 2000)
     } else {
-        setTimeout(() => askQuestion(newNumber), 2000)
+        setTimeout(() => {
+            askQuestion(newNumber);
+            rollBtn.disabled = false;
+        }, 2000)
     }
 }
 
@@ -58,11 +127,10 @@ const hideDice = () => {
     diceEl.style.visibility = 'hidden'
     diceEl.style.bottom = '0';
     diceEl.className = 'dice';
-    rollBtn.disabled = false;
 }
 
 const askQuestion = (newNumber) => {
-    questionModal.style.display = 'flex'
+    modal.style.display = 'flex'
     prepareQuestion(newNumber)
 }
 
@@ -112,11 +180,11 @@ const prepareQuestion = (newNumber) => {
     optionSubmit.innerHTML = "Submit";
     optionSubmit.onclick = () => checkAnswer(newNumber, questionIdx, questionOptions);
 
-    questionModal.append(questionTitle)
-    questionModal.append(questionImg)
-    questionModal.append(questionText)
-    questionModal.append(questionOptions)
-    questionModal.append(optionSubmit)
+    modal.append(questionTitle)
+    modal.append(questionImg)
+    modal.append(questionText)
+    modal.append(questionOptions)
+    modal.append(optionSubmit)
 }
 
 const checkAnswer = (newNumber, questionIdx, questionOptions) => {
@@ -128,8 +196,8 @@ const checkAnswer = (newNumber, questionIdx, questionOptions) => {
     })
 
     if (selectedValue) {
-        questionModal.style.display = 'none';
-        questionModal.innerHTML = '';
+        modal.style.display = 'none';
+        modal.innerHTML = '';
     }
 
     if (parseInt(selectedValue) === questions[questionIdx].correctAnswerId) {
@@ -140,7 +208,9 @@ const checkAnswer = (newNumber, questionIdx, questionOptions) => {
         showMessage('Incorrect answer!', false)
         points -= 0.5;
     }
+
     document.querySelector('#points').innerText = points;
+    saveUser();
 }
 
 const showMessage = (message, isSuccess, duration = 3000) => {
@@ -150,4 +220,15 @@ const showMessage = (message, isSuccess, duration = 3000) => {
     messageEl.style.color = isSuccess ? 'var(--success-color)' : 'var(--warning-color)'
 
     setTimeout(() => messageEl.style.display = 'none', duration)
+}
+
+const resetGame = () => {
+    const resetPwInput = document.getElementById('resetPw');
+    if (resetPwInput.value === 'gdku1230') {
+        localStorage.removeItem('gdku-board-game')
+        location.reload();
+    }
+}
+
+const savePlayerToAPI = () => {
 }
